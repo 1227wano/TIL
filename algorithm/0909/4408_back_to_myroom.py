3f7390ed240b61a76f77f168432b3fadd3dc9040
1227wano <1227wanho@gmail.com>
2025-09-10 02:14:18 +0900

    실습문제풀이

--- algorithm/0909/4408_back_to_myroom.py
+++ algorithm/0909/4408_back_to_myroom.py
@@ -6,17 +6,26 @@ for T in range(1, T+1):
     visited = [False] * N        # 그 동선 통과 여부
     cnt = N                      # 학생수만큼 이동하고 종료
 
+    arr = sorted(arr, key=lambda x: (x[0], x[1]))   # (시작시간이 같다면 종료시간으로 정렬)
+
     while cnt > 0:
         end_room = 0            # 마지막 방문방
         for i in range(N):
             s, e = arr[i][0], arr[i][1]     # s = start_time, e = end_time
             if not visited[i]:
                 if end_room % 2 == 0 and s > end_room and end_room < e:
-                    end_room = e
+                    if s < e:
+                        end_room = e
+                    else:
+                        end_room = s
                 elif end_room % 2 == 1 and s > end_room+1 and end_room < e:
-                    end_room = e
+                    if s < e:
+                        end_room = e
+                    else:
+                        end_room = s
                 visited[i] = True
                 cnt -= 1
         time += 1
+
 
     print(f'#{T} {time}')
