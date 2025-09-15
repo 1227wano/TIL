16adddcbd725b73e1f96ab51e321ad987a53da46
1227wano <1227wanho@gmail.com>
2025-09-16 01:55:00 +0900

    실습문제풀이

--- algorithm/0915/5248_divideng_gruop.py
+++ algorithm/0915/5248_divideng_gruop.py
@@ -1,6 +1,15 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+
+def dfs(node):
+    for next_node in li[node]:
+        if visited[next_node]:
+            continue
+        visited[next_node] = True
+        dfs(next_node)
+
+
 T = int(input())
 for T in range(1, T+1):
     V, E = map(int, input().split())
@@ -12,27 +21,12 @@ for T in range(1, T+1):
     for i in range(E):
         li[arr[i*2]].append(arr[i*2+1])
         li[arr[i*2+1]].append(arr[i*2])
-    print(li)
-    # 0
-    # 1 [2]v
-    # 2 [1, 3]v
-    # 3 [2]v
-    # 4 [5]v
-    # 5 [4]v
+
     visited = [False] * (V+1)
     cnt = 0
     for j in range(1, V+1):
-        if len(li[j]) > 0 and not visited[j]:
-            visited[j] = True
+        if not visited[j]:
             cnt += 1
-            for k in range(len(li[j])):
-                visited[li[j][k]] = True
-        elif len(li[j]) > 0 and visited[j]:
-            for k in range(len(li[j])):
-                visited[li[j][k]] = True
-
-    for l in range(1, V+1):
-        if not visited[l]:
-            cnt += 1
+            dfs(j)
 
     print(f'#{T} {cnt}')
