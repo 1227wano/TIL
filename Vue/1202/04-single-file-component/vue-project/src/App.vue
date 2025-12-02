fd9641c6db903238be7c1b7864bd48d38849880b
1227wano <1227wanho@gmail.com>
2025-12-02 17:35:31 +0900

    TIL: Vue Single File Component

---
+++ Vue/1202/04-single-file-component/vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>App.vue</h1>
+  <MyComponent />
+  <!-- <my-component>이렇게도 가능</my-component> -->
+</template>
+
+<script setup>
+// @ 는 src/ 를 가리키는 별칭
+import MyComponent from '@/components/MyComponent.vue'
+  
+</script>
+
+<style scoped>  
+/* 위의 scopoed 를 빼면 전역 스타일이 된다. */
+h1 {
+  color: blue;
+}
+</style>
